7e9ad18031f2da56972f29bb409463bdf5c7bec9
Olga Filipova <chudaol@gmail.com>
2015-01-17 22:43:27 +0100

    ref payment: added liqpay button and liqpay processor.

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -81,13 +81,17 @@ PIPELINE_SASS_ARGUMENTS = '--debug-info --require {proj_dir}/static/sass/bourbon
 FEATURES['AUTOMATIC_VERIFY_STUDENT_IDENTITY_FOR_TESTING'] = True
 FEATURES['ENABLE_PAYMENT_FAKE'] = True
 
-CC_PROCESSOR_NAME = 'CyberSource2'
+CC_PROCESSOR_NAME = 'LiqPay'
 CC_PROCESSOR = {
     'CyberSource2': {
         "PURCHASE_ENDPOINT": '/shoppingcart/payment_fake/',
         "SECRET_KEY": 'abcd123',
         "ACCESS_KEY": 'abcd123',
         "PROFILE_ID": 'edx',
+    },
+    'LiqPay': {
+         "PURCHASE_ENDPOINT": 'https://www.liqpay.com/api/checkout',
+         "PUBLIC_KEY": "i36499461501"
     }
 }
 

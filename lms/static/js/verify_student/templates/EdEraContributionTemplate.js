133503a7313d80d22e2d07df6570c8698c75f456
Olga Filipova <chudaol@gmail.com>
2015-01-29 03:00:08 +0100

    ref payment styling: added more styles and edited confirmation email template.

--- lms/static/js/verify_student/templates/EdEraContributionTemplate.js
+++ lms/static/js/verify_student/templates/EdEraContributionTemplate.js
@@ -1,11 +1,22 @@
 ;
 (function (define, undefined) {
     define(["underscore"], function (_) {
-        var html, decision;
+        var html, decision, free, mailLink;
 
         decision = '<div class="decision">Ви вирішили оплатити <span class="chosen_price" data-chosen="<%=chosen_price%>"><%=chosen_price%></span> гривень.</div>';
 
+        mailLink = '<a href="mailto=info@ed-era.com?subject=Мотиваційний лист для запису на <%=course_name%>&body=Мотиваційний лист від <%=user_full_name%>">' +
+                     'Надішліть нам мотиваційний лист' +
+                    '</a>'
+
+        free = '<div class="free">' +
+        'З огляду на різні ситуації, які можуть траплятися у житті, та на події в країні, ми надаємо можливість безкоштовного навчання. ' +
+        'Таке право мають передусім громадяни пільгових категорії та переселенці з зони АТО. ' +
+         mailLink +', у якому опишіть свою ситуацію, та додайте до нього відповідні документи, якщо це можливо.' +
+        '</div>';
+
         html = '<div class="contributions-row"></div>' +
+                free +
                 decision;
 
         return _.template(html);
